184f12bad290b459d5a13fb0498935e61c9c07d0
rchhokar814 <89408560+rchhokar814@users.noreply.github.com>
2021-09-16 19:31:42 -0400

    fetching data use vue

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,18 +1,25 @@
-const User = {
-    data() {
-      return {
-        "person": {},
-        };
-    },
-    created() {
-      fetch('https://randomuser.me/api')
-      .then( response => response.json())
-      .then( function (parsedJson){
-          this.person = parsedJson.results[0]         
+const Offer = {
+  data() {
+    return {
+      "person": {},
+      }
+  },
+  created() {
+      console.log("A");
+
+      fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then((parsedJson) => {
+          console.log(parsedJson);
+          this.person = parsedJson.results[0]
+          console.log("C");
       })
       .catch( err => {
-        console.error( error)
-      })     
+          console.error(err)
+      })
+
+      console.log("B");
   }
-}  
-Vue.createApp(User).mount('#userApp');
+}
+
+Vue.createApp(Offer).mount('#offerApp');
