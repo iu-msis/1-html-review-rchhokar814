5be25aad7e6d3fcfb934f9a5cab62b33a2194e84
rchhokar814 <89408560+rchhokar814@users.noreply.github.com>
2021-10-20 11:24:40 -0400

    getting forms to work

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -1,51 +1,50 @@
-  const Rough = {
-    data() {
-      return {
-        "books": {},
-        bookForm: {}
-        }
-    },
-  
-  
-   methods: {
-      fetchBookData() {
-        fetch('/api/book/')
-        .then(response => response.json())
-        .then((parsedJson) => 
-        {
-           console.log(parsedJson);
-            this.books = parsedJson
-        })
-  
-        .catch( err => 
-        {
-            console.error(err)
-        })
+const Rough = {
+  data() {
+    return {
+      "books": {},
+      bookForm: {}
       }
-    },
+  },
 
-    postNewOffer(evt) {
-      fetch('api/book/create.php', {
-          method:'POST',
-          body: JSON.stringify(this.bookForm),
-          headers: {
-            "Content-Type": "application/json; charset=utf-8"
-          }
-        })
-        .then( response => response.json() )
-        .then( json => {
-          console.log("Returned from post:", json);
-          // TODO: test a result was returned!
-          this.books = json;
-          
-          // reset the form
-          this.bookForm = {};
-        });
-    },
 
-  created() {
-      this.fetchBookData();
-    }
+ methods: {
+    fetchBookData() {
+      fetch('/api/book/')
+      .then(response => response.json())
+      .then((parsedJson) => 
+      {
+         console.log(parsedJson);
+          this.books = parsedJson
+      })
+
+      .catch( err => 
+      {
+          console.error(err)
+      })
+    },
+  postNewBook(evt){
+    fetch('api/book/create.php', {
+        method:'POST',
+        body: JSON.stringify(this.bookForm),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      .then( response => response.json() )
+      .then( json => {
+        console.log("Returned from post:", json);
+        // TODO: test a result was returned!
+        this.books = json;
+        
+        // reset the form
+        this.bookForm = {};
+      });
+      
   }
-  
-  Vue.createApp(Rough).mount('#roughApp');
+ },
+created() {
+    this.fetchBookData();
+  }
+}
+
+Vue.createApp(Rough).mount('#roughApp');
